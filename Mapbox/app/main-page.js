58c963e16c39fd628a80bf6e0285b4cb6a40d6ab
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-10-12 22:54:55 +0200

    Superawesome XML-declared maps added!

--- Mapbox/app/main-page.js
+++ Mapbox/app/main-page.js
@@ -4,3 +4,22 @@ function pageLoaded(args) {
   page.bindingContext = vmModule.mainViewModel;
 }
 exports.pageLoaded = pageLoaded;
+
+var mapbox = require("nativescript-mapbox");
+
+function onMapReady(args) {
+  args.map.addMarkers([
+    {
+      id: 2,
+      lat: 52.3602160,
+      lng: 4.8891680,
+      title: 'One-line title here', // no popup unless set
+      subtitle: 'Really really nice location',
+      iconPath: 'res/markers/green_pin_marker.png',
+      onTap: function(){console.log("'Nice location' marker tapped");},
+      onCalloutTap: function(){console.log("'Nice location' marker callout tapped");}
+    }]
+  );
+}
+
+exports.onMapReady = onMapReady;
--- Mapbox/app/main-view-model.js
+++ Mapbox/app/main-view-model.js
@@ -3,6 +3,7 @@ var mapbox = require("nativescript-mapbox");
 var dialogs = require("ui/dialogs");
 var platform = require("platform");
 var isIOS = platform.device.os === platform.platformNames.ios;
+
 var DemoAppModel = (function (_super) {
   __extends(DemoAppModel, _super);
   function DemoAppModel() {
